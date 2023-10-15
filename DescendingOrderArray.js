d3cdd13f1a6cf9451c4e5aa582077f5a9605a4d5
salunkepratik <147967577+salunkepratik@users.noreply.github.com>
2023-10-15 13:18:27 +0530

    Create DescendingOrderArray.js

---
+++ DescendingOrderArray.js
@@ -0,0 +1,13 @@
+<html>
+  <head>
+  <body>
+  <h1>Sorting Array in Descending Order</h1>
+  <script>
+   let Array = [100,70,44,85,20,65,10];
+   console.log("Unsorted Array: ",Array)
+   Array.sort((a,b) => a-b);
+    console.log("Sorted Array: ",Array)
+  </script>
+ </body>
+</head>
+</html>
